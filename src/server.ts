165a7bd0e01e95df73f50d636327e7c7edb418b4
jon <jon@jons-MacBook-Pro.local>
2020-12-03 10:59:04 -0600

    declare error processes

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,30 @@
 import http from "http";
 import express from "express";
-import { applyMiddleware, applyRoutes } from "./utils";
-import middleware from "./middleware";
-import routes from "./services";
+import { applyMiddleware, applyRoutes } from "./utils/index";
+import middleware from "./middleware/index";
+import routes from "./services/index";
 
-const router = express();
-applyMiddleware(middleware, router);
-applyRoutes(routes, router);
+//error handling
+process.on("uncaughtException", e => {
+    console.log(e);
+    process.exit(1);
+});
 
+process.on("unhandledRejection", e => {
+    console.log(e);
+    process.exit(1);
+});
+
+//create express app
+const app = express();
+
+//connect middleware and apply routes
+applyMiddleware(middleware, app);
+applyRoutes(routes, app);
+
+//declare PORT and initialize server
 const { PORT = 3000 } = process.env;
-const server = http.createServer(router);
+const server = http.createServer(app);
 
 server.listen(PORT, () =>
     console.log(`Server is running http://localhost:${PORT}...`)
