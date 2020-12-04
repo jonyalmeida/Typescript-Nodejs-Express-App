32dd0e099ce29461ff713dfd17df5392b9c8498e
jon <jon@jons-MacBook-Pro.local>
2020-12-03 23:48:27 -0600

    update search controller & routes

--- src/services/search/routes.ts
+++ src/services/search/routes.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
 import { getPlacesByName } from "./SearchController";
 import { checkSearchParams } from "../../middleware/checks";
-import { authenticate } from '../../middleware/authenticate;'
+import { authenticate } from '../../middleware/authenticate';
+import { getFromCache } from "../../middleware/caching";
 
 export default [
     {
@@ -9,10 +10,24 @@ export default [
         method: "get",
         handler: [
             checkSearchParams,
+            getFromCache,
             async ({ query }: Request, res: Response) => {
-                const result = await getPlacesByName(query.q);
+                const result = await getPlacesByName(query.q as string);
                 res.status(200).send(result);
-            }
-        ]
-    }
+            },
+        ],
+    },
+    {
+        path: "/api/v1/search/protected",
+        method: "get",
+        handler: [
+            authenticate,
+            checkSearchParams,
+            getFromCache,
+            async ({ query }: Request, res: Response) => {
+                const result = await getPlacesByName(query.q as string);
+                res.status(200).send(result)
+            },
+        ],
+    },
 ];
