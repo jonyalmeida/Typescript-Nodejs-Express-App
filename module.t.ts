6606192d7324ce52172a8cdb7623113fd4b80746
jon <jon@jons-MacBook-Pro.local>
2020-12-04 00:19:05 -0600

    update server.ts

---
+++ module.t.ts
@@ -0,0 +1,6 @@
+type Feature = {
+	properties: {
+		confidence: number;
+		};
+	};
+
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,27 @@
 import http from "http";
 import express from "express";
+import "express-async-errors";
 import { applyMiddleware, applyRoutes } from "./utils/index";
 import middleware from "./middleware/index";
 import errorHandlers from "./middleware/errorHandlers";
 import routes from "./services/index";
+import { initDependecies } from "./config/index";
+import { logger } from "./config/logger";
 
 //error handling
 process.on("uncaughtException", e => {
-    console.log(e);
+    logger.error({
+        message: `uncaughtException`,
+        extra: e,
+    });
     process.exit(1);
 });
 
 process.on("unhandledRejection", e => {
-    console.log(e);
+    logger.error({
+        message: `unhandledRejection`,
+        extra: e,
+    });
     process.exit(1);
 });
 
@@ -28,6 +37,13 @@ applyMiddleware(errorHandlers, router)
 const { PORT = 3000 } = process.env;
 const server = http.createServer(router);
 
-server.listen(PORT, () =>
-    console.log(`Server is running http://localhost:${PORT}...`)
-);
+async function start() {
+    await initDependecies();
+    server.listen(PORT, () =>
+        logger.info({
+            message: `Server is running http://localhost:${PORT}...`,
+        }),
+    );
+}
+
+start()
